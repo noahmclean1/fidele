703b772b496c130987fe15c62940dcd5860cb1e2
Valerie Han <valeriehan@uchicago.edu>
2019-09-25 20:55:47 -0500

    Added instructions on underlining part of cell in Excel file

--- program2.py
+++ program2.py
@@ -1,6 +1,11 @@
 import sys
 from docx import Document # may need to install this package! 
 import xlrd
+import xlsxwriter
+#CREATE EXCEL FILE CALLED test.xlsx
+workbook = xlsxwriter.Workbook('test.xlsx') 
+worksheet = workbook.add_worksheet()
+underline = workbook.add_format({'underline': True})
 
 input_file = sys.argv[1]
 
@@ -27,3 +32,17 @@ sheet = wb.sheet_by_index(0)
 # For row 0 and column 0 
 sheet.cell_value(0, 0).encode("ascii") 
 '''
+
+'''
+Underlining parts of cells in Excel
+
+import xlsxwriter
+workbook = xlsxwriter.Workbook('test.xlsx')
+worksheet = workbook.add_worksheet()
+underline = workbook.add_format({'underline': True})
+worksheet.write_rich_string(0, 0, 'under', underline, 'line') # 'line' is underlined, 'under' is not.
+worksheet.write_rich_string('A2', underline, 'under', 'line') # 'under' is underlined, 'line' is not.
+workbook.close()
+'''
+
+workbook.close()
