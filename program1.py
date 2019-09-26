30cf67181e05396229030d1dd6cc1a1a9e2efefd
U-LAPTOP-AKPC20\AK <AK Alilonu>
2019-09-25 21:45:00 -0500

    added code to program1

--- program1.py
+++ program1.py
@@ -11,3 +11,17 @@ with open(input_file, 'rb') as f:
 		fullText.append(para.text)
 	print('\n'.join(fullText))
 	#source_stream = StringIO(f.read())
+
+# put lines of text into an array called lines 
+num_lines = len(lines)
+i = 0
+while i != num_lines - 1:
+    # number of occurrences of lines[i] = 1
+    line = lines[i]
+    j = i + 1
+    while j != num_lines - 1:
+        if lines[j] == line:
+            # underline lines[j]
+            # increment number of occurrences of that line
+        j++
+    i++
