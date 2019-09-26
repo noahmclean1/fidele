f2e7428a25de163903032cc458f3c5abc5085d62
Noah McLean <nmclean@uchicago.edu>
2019-09-25 20:40:41 -0500

    added xlrd sample code

--- program2.py
+++ program2.py
@@ -1,6 +1,6 @@
 import sys
 from docx import Document # may need to install this package! 
-import csv
+import xlrd
 
 input_file = sys.argv[1]
 
@@ -11,4 +11,19 @@ with open(input_file, 'rb') as f:
 		fullText.append(para.text)
 	print('\n'.join(fullText))
 
-# To read/write into CSV files, use csv.reader / csv.writer
+# To read/write into excel files, look at the following snippet:
+
+'''
+# Reading an excel file using Python 
+import xlrd 
+  
+# Give the location of the file 
+loc = ("path of file") 
+  
+# To open Workbook 
+wb = xlrd.open_workbook(loc) 
+sheet = wb.sheet_by_index(0) 
+  
+# For row 0 and column 0 
+sheet.cell_value(0, 0).encode("ascii") 
+'''
