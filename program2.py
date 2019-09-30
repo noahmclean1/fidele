92e45f7c126db8753005c989a2952899ceb62858
Noah McLean <nmclean@uchicago.edu>
2019-09-29 19:13:35 -0500

    program 2 in semi-working order, has some bugs

--- program2.py
+++ program2.py
@@ -4,12 +4,25 @@ import xlrd
 import xlwt 
 import xlsxwriter
 #CREATE EXCEL FILE CALLED test.xlsx
-workbook = xlsxwriter.Workbook('test.xlsx') 
+workbook = xlsxwriter.Workbook('referenceTable.xlsx') 
 worksheet = workbook.add_worksheet()
 underline = workbook.add_format({'underline': True})
 
 input_file = sys.argv[1]
 freq_file = sys.argv[2]
+
+#-----------------------------------------
+# Function(s)
+
+def listToString(li):
+	fullString = ""
+	for i,unit in enumerate(li):
+		if i == 0:
+			fullString = unit
+		fullString = fullString + "," + unit
+	return fullString
+
+#-----------------------------------------
 
 '''
 with open(input_file, 'rb') as f:
@@ -50,7 +63,7 @@ worksheet.write_rich_string('A2', underline, 'under', 'line') # 'under' is under
 workbook.close()
 '''
 
-
+print("Reading input file...")
 # Open the files
 unitListFile = open(input_file, 'rb')
 ulf = Document(unitListFile)
@@ -58,49 +71,65 @@ unitlist = []
 for para in ulf.paragraphs:
 	unitlist.append(para.text)
 
-freqListFile = open(freq_file, 'rb')
-flf = Document(freqListFile)
-freqlist = []
-for para in flf.paragraphs:
-	freqlist.append(para.text)
+print("Reading frequency list into memory...")
+# Open and read in the frequency table from the excel file from program 1
+freqListFile = xlrd.open_workbook(freq_file)
+sheet = freqListFile.sheet_by_index(0)
+freqTable = {}
+for i in range(0,sheet.nrows):
+	freqTable[sheet.cell_value(i,0)] = sheet.cell_value(i,1)
 
 # Create Excel file to write into
 
 # For each line:
+i = 0
 for units in unitlist:
 	# Tokenize the units string
 	toks = units.split(",")
 
 	# Search for most frequent unit (down frequency list) -> dubbed B
 	topword = None
-	for freqword in freqlist:
-		for token in toks:
-			if freqword == token:
+	topnum = 0
+	for token in toks:
+		if token in freqTable:
+			if freqTable[token] > topnum:
+				topnum = freqTable[token]
 				topword = token
-				break
-		if topword != None:
-			break
 
 	# Check if none of the words were found, if so, write the whole line WITHOUT underlines and move on
 	if topword == None:
 		# Write whole line in alphabetical order
-		rest = toks.sort()
-		# WRITE
+		rest = toks
+		rest.sort()
+		worksheet.write_rich_string(i,0,listToString(rest))
+		i += 1
+		continue
+
+	# Special case: topword is the only one?
+	if len(toks) == 1:
+		worksheet.write_rich_string(i,0,underline,topword)
+		worksheet.write(i,1,topword)
+		i += 1
 		continue
 
 	# Remove B from the read line, sort the rest
-	rest = toks.remove(topword)
-	rest = rest.sort()
+	rest = toks
+	rest.remove(topword)
+	rest.sort()
+	restString = listToString(rest)
 
 	# Write B,[REST OF LINE] with B underlined into XLSX
+	worksheet.write_rich_string(i,0,underline,topword,restString)
+
 	# Write B again, normally, into the right column
+	worksheet.write(i,1,topword)
+	i += 1
 
 # def mainstream(word_list, freq_list):
     
 
 
 
-workbook.save()
 workbook.close()
 
 
