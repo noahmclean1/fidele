1ec0c7b25cc65174b95381c2c2a065efdd0925c2
U-LAPTOP-AKPC20\AK <AK Alilonu>
2020-01-30 16:48:51 -0600

    No git cloning

--- first_download.bat
+++ first_download.bat
@@ -1,5 +1,4 @@
-ECHO Cloning GitHub repository ...
-git clone https://github.com/noahmclean1/fidele.git
+REM git clone https://github.com/noahmclean1/fidele.git
 ECHO Installing packages ...
 python3 -m pip install python-docx
 python3 -m pip install xlsxwriter
